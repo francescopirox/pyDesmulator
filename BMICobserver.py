fd788ff9b91feca5b4bc81bdc733fdbb6e9ced7f
francescopirox <f.pironti1999@gmail.com>
2022-05-16 23:53:01 +0200

    Ic started

--- BMICobserver.py
+++ BMICobserver.py
@@ -21,22 +21,27 @@ class ObserverTime:
 
 class Ic:
     mean_value: float
-    quantile:float
+    quantile: float
+
+    def __init__(self, mean_value, quantile) -> None:
+        self.quantile = quantile
+        self.mean_value = mean_value
 
 
-
-class BMICobserver(Observer):
+class BMICObserver(Observer):
     dump: int
     transitorio: bool = True
     time_start_data: int = -1
     batch_size: int
+    batch_number: int
     observers: ObserverTime = []
 
-    def __init__(self, batch_size: int, batch_number) -> None:
+    def __init__(self, batch_size: int, batch_number: int) -> None:
         super().__init__()
         self.dump = 0
         self.transitorio = False
         self.batch_size = batch_size
+        self.batch_number = batch_number
 
     def __init__(self, dump: int, batch_size: int, batch_number: int) -> None:
         super().__init__()
@@ -45,32 +50,46 @@ class BMICobserver(Observer):
 
     def client_arrival(self, time: int):
         self.client_arrived += 1
-        if (self.client_arrived == self.dump):
+        if self.client_arrived == self.dump:
             self.transitorio = False
             self.observers.append(ObserverTime(time, Observer()))
             self.observers[-1].observer.client_arrival(time - self.observers[-1].time_stamp)
+        if not self.transitorio:
+            pass
 
     def client_departure(self, time):
-        self.observers[-1].observer.client_departure(time - self.observers[-1].time_stamp)
+        if not self.transitorio:
+            self.observers[-1].observer.client_departure(time - self.observers[-1].time_stamp)
 
     def client_service_start(self, time):
-        self.observers[-1].observer.client_service_start(time)
+        if not self.transitorio:
+            self.observers[-1].observer.client_service_start(time)
 
     def client_service_stop(self, time):
-        self.observers[-1].observer.client_service_stop(time)
+        if not self.transitorio:
+            self.observers[-1].observer.client_service_stop(time)
 
     def get_waiting_time_ic(self, stop_time):
         values = []
-        mean_value: float = 0
-        std_dev: float = 0
-        n: int = 0
         for observertype in self.observers:
             values.append(observertype.observer.get_waiting_time())
-            mean_value += observertype.observer.get_waiting_time()
-        n = len(values)
-        mean_value = mean_value / n
-        for value in values:
-            std_dev += (value - mean_value) ^ 2
-        std_dev /= (n - 1)
-        std_dev = math.sqrt(std_dev)
-        return Ic(mean_value, std_dev/math.sqrt(n))
+
+        return self.confidence_interval(values)
+
+    def confidence_interval(self, values):
+        if self.state():
+            mean_value: float = 0
+            n = len(values)
+            std_dev: float = 0
+            n: int = 0
+            for value in values:
+                mean_value += value
+            mean_value = mean_value / n
+            for value in values:
+                std_dev += (value - mean_value) ^ 2
+            std_dev /= (n - 1)
+            std_dev = math.sqrt(std_dev)
+            return Ic(mean_value, std_dev / math.sqrt(n))
+
+    def state(self):
+        return self.client_arrived > self.dump + self.batch_size * self.batch_number
---
+++ mm1-ic.py
@@ -0,0 +1,36 @@
+import observer
+from clientgenerator import ClientGenerator
+from exponentialDistribution import ExponentialDistribution
+from BMICobserver import BMICObserver
+from simulator import Simulator
+from singleserverstation import SingleServerStation
+
+print("simulatore coda m/m/1")
+lamb = input("inserisci lambda:     ")
+mu = input("inserisci mu:         ")
+d_lamb = ExponentialDistribution(float(lamb))
+d_mu = ExponentialDistribution(float(mu))
+
+time = int(input("inserisci il tempo di simulazione: "))
+total_time = time
+s = Simulator(time)
+s.trace = False
+o = BMICObserver(2000,2000,30)
+st = SingleServerStation(o, s, d_mu)
+gen = ClientGenerator(st, None, s, d_lamb)
+
+print("**SIMULAZIONE IN CORSO** \n\n")
+s.start_simulation()
+print("\n")
+st.print_state()
+print(o.state())
+time = int(input("inserisci il nuovo tempo di simulazione per continuare, -1 per completare: "))
+
+while time > 0:
+    total_time += time
+    s.resume_simulation(time)
+    print("**SIMULAZIONE IN CORSO** \n \n")
+    o.ouput(total_time)
+    print("\n")
+    st.print_state()
+    time = int(input("inserisci il nuovo tempo di simulazione per continuare, -1 per completare: "))
